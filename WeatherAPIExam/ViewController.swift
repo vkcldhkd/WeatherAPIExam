3c64480cb1bacc3dae446deb054c113a1dece51b
성현 <vkcldhkd@gmail.com>
2019-08-04 21:33:01 +0900

    Initial Commit

---
+++ WeatherAPIExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAPIExam
+//
+//  Created by Sung Hyun on 04/08/2019.
+//  Copyright © 2019 SungHyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
